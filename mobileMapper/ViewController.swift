a8da21ba4944b8a60792f1ed297368acecfdce73
Olivia Mellen <omellen2269@jh-mba158-18.local>
2019-03-14 14:46:49 -0500

    created pins with detail button

--- mobileMapper/ViewController.swift
+++ mobileMapper/ViewController.swift
@@ -9,13 +9,13 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController, CLLocationManagerDelegate
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate
 {
 
     @IBOutlet weak var mapView: MKMapView!
     let locationManager = CLLocationManager()
     var currentLocation: CLLocation!
-    var parks:[MKMapItem] = []
+    var parks: [MKMapItem] = []
     
     override func viewDidLoad()
     {
@@ -25,7 +25,23 @@ class ViewController: UIViewController, CLLocationManagerDelegate
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.startUpdatingLocation()
+        
+        mapView.delegate = self
     }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView?
+    {
+        if annotation.isEqual(mapView.userLocation)
+        {
+            return nil
+        }
+        let pin = MKPinAnnotationView(annotation: annotation, reuseIdentifier: nil)
+        pin.canShowCallout = true
+        let button = UIButton(type: .detailDisclosure)
+        pin.rightCalloutAccessoryView = button
+        return pin
+    }
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         currentLocation = locations[0]
     }
@@ -42,11 +58,18 @@ class ViewController: UIViewController, CLLocationManagerDelegate
     {
         let request = MKLocalSearch.Request()
         request.naturalLanguageQuery = "parks"
-        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
         request.region = MKCoordinateRegion(center: currentLocation.coordinate, span: span)
         let search = MKLocalSearch(request: request)
         search.start { (response, error) in
-            <#code#>
+            guard let response = response else { return }
+            for mapItem in response.mapItems {
+                self.parks.append(mapItem)
+                let annotation = MKPointAnnotation()
+                annotation.title = mapItem.name
+                annotation.coordinate = mapItem.placemark.coordinate
+                self.mapView.addAnnotation(annotation)
+            }
         }
     }
     
